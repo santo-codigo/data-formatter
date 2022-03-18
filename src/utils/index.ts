80a66bac7c4f6b9c2f781105e702497c678bb380
Lucas <lucas.santos@pagtel.com.br>
2022-03-18 19:12:05 -0300

    feat: add utility to check promises

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
 export const capitalize = (s: string) => {
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
+
+export const isPromise = (obj: any) => {
+  return (
+    !!obj &&
+    (typeof obj === "object" || typeof obj === "function") &&
+    typeof obj.then === "function"
+  );
+};
